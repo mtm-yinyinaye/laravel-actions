5befdd251f5b1186ff46973097cf92373ffc033f
mtm-yinyinaye <yinyinaye@metateammyanmar.com>
2023-10-23 09:40:42 +0630

    chages s3 bucket

--- depoly/scripts/after-install.sh
+++ depoly/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /var/www/html/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://dev05-test/SpringBootHelloWorldExampleApplication.war /var/www/html/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
