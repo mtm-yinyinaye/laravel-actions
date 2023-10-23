1386a5ce2706c38aaa916dc32a9116bf83577123
mtm-yinyinaye <yinyinaye@metateammyanmar.com>
2023-10-23 11:16:51 +0630

    fix deploy faill

--- depoly/scripts/after-install.sh
+++ depoly/scripts/after-install.sh
@@ -1,10 +1,14 @@
+# #!/bin/bash
+# set -xe
+
+
+# # Copy war file from S3 bucket to tomcat webapp folder
+# aws s3 cp s3://dev05-test/SpringBootHelloWorldExampleApplication.war /var/www/html/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+
+
+# # Ensure the ownership permissions are correct.
+# chown -R tomcat:tomcat /var/www/html/tomcat9/webapps
+
 #!/bin/bash
-set -xe
-
-
-# Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://dev05-test/SpringBootHelloWorldExampleApplication.war /var/www/html/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
-
-
-# Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /var/www/html/tomcat9/webapps
+source /var/www/html/laravel-app-testing/.profile
+chown -R ubuntu:ubuntu /var/www/html/laravel-app-testing
--- depoly/scripts/application-start.sh
+++ depoly/scripts/application-start.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-set -xe
+# #!/bin/bash
+# set -xe
 
-# Start Tomcat, the application server.
-service tomcat start
+# # Start Tomcat, the application server.
+# service tomcat start
+
+
+#!/bin/bash
+source /var/www/html/ .profile
+cd /var/www/html
+chmod +x ./startup-script.sh
+./startup-script.sh > deploy.log
